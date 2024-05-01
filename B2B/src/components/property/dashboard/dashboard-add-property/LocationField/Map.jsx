b47774558913a3443250b4970bbddb30e3f0c2d2
unknown <chinidhia@gmail.com>
2024-05-01 19:40:06 +0200

    integration mapbox offer details done

--- B2B/src/components/property/dashboard/dashboard-add-property/LocationField/Map.jsx
+++ B2B/src/components/property/dashboard/dashboard-add-property/LocationField/Map.jsx
@@ -1,13 +1,47 @@
-const Map = () => {
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from 'mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYW5vaXJtYXAiLCJhIjoiY2xybnQzeGRiMDI2dDJqbnhtcng4NWk2ayJ9.gpX-sPWKgk03UjvmPzIW_A';
+
+function Map({ setLatInput, setLngInput }) {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const marker = useRef(null);
+  const [lng, setLng] = useState(9.5375); // Center longitude for Tunisia
+  const [lat, setLat] = useState(33.8869); // Center latitude for Tunisia
+  const [zoom, setZoom] = useState(6);
+
+  useEffect(() => {
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [lng, lat], // Set initial center to Tunisia
+      zoom: zoom
+    });
+
+    marker.current = new mapboxgl.Marker();
+
+    map.current.on('click', (e) => {
+      const { lngLat } = e;
+      const clickedLng = lngLat.lng;
+      const clickedLat = lngLat.lat;
+
+      setLng(clickedLng);
+      setLat(clickedLat);
+      setLngInput(clickedLng);
+      setLatInput(clickedLat);
+
+      marker.current.setLngLat([clickedLng, clickedLat]);
+      marker.current.addTo(map.current);
+    });
+  }, []);
+
   return (
-    <iframe
-      className="h550"
-      loading="lazy"
-      src="https://maps.google.com/maps?q=Algeria&t=m&z=6&output=embed&iwloc=near"
-      title="Algeria"
-      aria-label="Algeria"
-    />
+    <div>
+      <div ref={mapContainer} style={{ height: "400px" ,borderRadius:"15px"}} />
+    </div>
   );
-};
+}
 
 export default Map;
--- B2B/src/components/property/dashboard/dashboard-add-property/LocationField/index.jsx
+++ B2B/src/components/property/dashboard/dashboard-add-property/LocationField/index.jsx
@@ -1,51 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import SelectMulitField from "./SelectMulitField";
 import Map from "./Map";
 
 const LocationField = ({ n, f }) => {
+  const [latInput, setLatInput] = useState();
+  const [lngInput, setLngInput] = useState();
   return (
     <form className="form-style1">
       <div className="row">
-        {/* <div className="col-sm-12">
-          <div className="mb20">
-            <label className="heading-color ff-heading fw600 mb10">
-              Address
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Your Name"
-            />
-          </div>
-        </div> */}
-        {/* End col-12 */}
-
-        {/* <SelectMulitField />
-
-        <div className="col-sm-6 col-xl-4">
-          <div className="mb20">
-            <label className="heading-color ff-heading fw600 mb10">Zip</label>
-            <input type="text" className="form-control" />
-          </div>
-        </div> */}
-        {/* End col-4 */}
-        {/* 
-        <div className="col-sm-6 col-xl-4">
-          <div className="mb20">
-            <label className="heading-color ff-heading fw600 mb10">
-              Neighborhood
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Neighborhood"
-            />
-          </div>
-        </div> */}
-        {/* End col-4 */}
-
         <div className="col-sm-12">
-          <Map />
+          <Map setLngInput={setLngInput} setLatInput={setLatInput} />
         </div>
         {/* End col-12 */}
       </div>
@@ -57,7 +21,7 @@ const LocationField = ({ n, f }) => {
             <label className="heading-color ff-heading fw600 mb10">
               Latitude
             </label>
-            <input type="text" className="form-control" />
+            <input type="text" value={latInput} className="form-control" />
           </div>
         </div>
         {/* End .col-sm-6 */}
@@ -67,7 +31,7 @@ const LocationField = ({ n, f }) => {
             <label className="heading-color ff-heading fw600 mb10">
               Longitude
             </label>
-            <input type="text" className="form-control" />
+            <input type="text" value={lngInput} className="form-control" />
           </div>
         </div>
         <div className="col-sm-6 col-xl-4">
